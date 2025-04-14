a8e3098c57a045fc0b708151d230afba0c8a9113
nhan <trongnhandu2003@gmail.com>
2025-04-14 20:33:11 +0700

    Add ChuongTrinhDaoTao

---
+++ src/main/java/com/example/CTDT_APP/dto/request/ChuongTrinhDaoTaoUpdateRequest.java
@@ -0,0 +1,24 @@
+package com.example.CTDT_APP.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ChuongTrinhDaoTaoUpdateRequest {
+    @NotBlank(message = "Tên CTDT không được để trống")
+    private String tenCTDT;
+
+    private String moTa;
+
+    @NotBlank(message = "Trạng thái không được để trống")
+    private String trangThai;
+
+    @NotBlank(message = "Mã bậc đào tạo không được để trống")
+    private String maBac;
+
+    @NotBlank(message = "Mã hình thức đào tạo không được để trống")
+    private String maHe;
+
+    @NotBlank(message = "Mã ngành đào tạo không được để trống")
+    private String maNganh;
+}
